d8df71a3f0c6317f3e0a57cead9a0ba61f893cdb
hskim2311 <hskim2311@gailab.co.kr>
2025-07-15 14:31:19 +0900

    2025. 07. 15.
    불법주라 리스트 컨트롤러 구현

--- src/main/java/com/disabled/controller/EventListController.java
+++ src/main/java/com/disabled/controller/EventListController.java
@@ -1,21 +1,28 @@
 package com.disabled.controller;
 
-import java.sql.Date;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-import org.apache.ibatis.annotations.Param;
+import org.apache.commons.collections.map.HashedMap;
 import org.egovframe.rte.ptl.mvc.tags.ui.pagination.PaginationInfo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.disabled.service.EventListService;
+
 @Controller
 @RequestMapping("/eventList")
 public class EventListController {
+	
+	@Autowired
+	EventListService eventListService;
 	
 	@RequestMapping("")
 	public String rootRedirect() {
