3957cfea70f2dece4c3d75357850707dfdac79fd
hskim2311 <hskim2311@gailab.co.kr>
2025-07-15 14:30:26 +0900

    2025. 07. 15.
    불법주차 리스트 컨트롤러 구현

--- src/main/java/com/disabled/controller/EventListController.java
+++ src/main/java/com/disabled/controller/EventListController.java
@@ -20,35 +20,115 @@ public class EventListController {
 	@RequestMapping("")
 	public String rootRedirect() {
 		
+		System.out.println("viewEventList redirect");
+		
 		return "redirect:/eventList/viewEventList.do";
 	}
 	
+	// 불법주차 리스트
 	@RequestMapping("/viewEventList.do")
 	public String viewEventList(
 			@RequestParam(value="startDate", required=false) String startDate
 			, @RequestParam(value="endDate", required=false) String endDate
 			, @RequestParam(value="searchKeyword", required=false) String searchKeyword
+			, @RequestParam(value="page", required=false) Integer page
 			, Model model) {
+		
+		System.out.println("viewEventList in");
 		
 		List<Map<String, Object>> eventList = new ArrayList<Map<String,Object>>();
 		
+		// 페이징 객체
 		PaginationInfo paginationInfo = new PaginationInfo();
+		
+		// 오늘 날짜 설정
+		LocalDate today = LocalDate.now();
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd");
+		
+		// 파라미터 값 설정
+		Map<String, Object> paramMap = new HashedMap();
 		
 		try {
 			
-			//특별한 검색 조건을 지정하지 않을 경우 시작일은 그 달의 첫째날로 초기화
-			// if 
+			//특별한 검색 조건을 지정하지 않을 경우 시작일은 그 달의 첫째날로 초기화, 마지막 날은 오늘로 초기화
+			if(startDate == null || startDate.isEmpty()) {
+				startDate = today.withDayOfMonth(1).format(formatter).toString();
+			}else {
+				startDate = startDate.formatted("yyyyMMdd").toString();
+			}
+			if(endDate == null || endDate.isEmpty()) {
+				endDate = today.format(formatter).toString();
+			}else {
+				endDate = endDate.formatted("yyyyMMdd").toString();
+			}
 			
-			// eventList = 
+			//현재 페이지가 없는 경우 1페이지로 설정
+			if(page == null || page < 0) {
+				page = 1;
+			}
+			
+			System.out.println("searchKeyword: "+ searchKeyword + " startDate: " + startDate + " endDate: " + endDate + " ");
+			
+			// 페이징 설정
+			paginationInfo.setCurrentPageNo(page); // 현제 페이지 번호
+			paginationInfo.setRecordCountPerPage(10);  // 한 페이지에 출력할 게시글 수
+			paginationInfo.setPageSize(10); // 페이지 블록 수
+			
+			int firstIndex = paginationInfo.getFirstRecordIndex(); // LIMIT offset
+			int recordCountPerPage = paginationInfo.getRecordCountPerPage();  //LIMIT count
+			
+			System.out.println("firstIndex: " + firstIndex);
+			System.out.println("recordCountPerPage: " + recordCountPerPage);
+			
+			// DB 검색을 위한 파라미터 설정
+			paramMap.put("firstIndex", firstIndex);
+			paramMap.put("recordCountPerPage", recordCountPerPage);
+			paramMap.put("searchKeyword", searchKeyword);
+			paramMap.put("startDate",startDate);
+			paramMap.put("endDate",endDate);
+			
+			System.out.println("getEventList");
+			
+			eventList = eventListService.getEventList(paramMap);
+			
 		} catch (Exception e) {
+			System.out.println("이벤트 리스트 컨트롤러 오류");
+			System.out.println(e + "");
 			// TODO: handle exception
 		}
+		
+		// model add
+		model.addAttribute("eventList", eventList);
+		model.addAttribute("searchKeyword", searchKeyword);
+		model.addAttribute("startDate",startDate);
+		model.addAttribute("endDate", endDate);
+		
+		System.out.println("return eventList");
 		
 		return "eventList/eventList";
 	}
 	
+	// 불법주차 리스트 상세
 	@GetMapping("/eventListDetail")
 	private String eventListDetail(Integer evId, Model model) {
+		
+		System.out.println("eventListDetail in");
+		
+		Map<String, Object> eventListDetail = new HashedMap();
+		
+		try {
+			
+			System.out.println("getEventListDetail");
+			
+			eventListDetail = eventListService.getEventListDetail(evId);
+		} catch (Exception e) {
+			System.out.println("이벤트 리스트 상세 컨트롤러 오류");
+			// TODO: handle exception
+		}
+		
+		model.addAttribute("eventListDetail", eventListDetail);
+		
+		System.out.println("return eventListDetail");
 		
 		return "eventList/eventListDetail";
 	}
