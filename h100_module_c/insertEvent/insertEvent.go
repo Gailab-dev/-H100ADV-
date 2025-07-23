34df7eb6922d979196bfe03abb774ed41280a527
hskim2311 <hskim2311@gailab.co.kr>
2025-07-23 15:57:03 +0900

    device에서 insertEvent 요청시 json파일 decoding하여 데이터 insert하는 모듈 추가

---
+++ h100_module_c/insertEvent/insertEvent.go
@@ -0,0 +1,52 @@
+package insertEvent
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"local.dev/h100_module_c/database"
+)
+
+type Tbl_Event_Data struct {
+	EvId       uint   `gorm:"primaryKey" json:"id"`
+	EvDvId    uint   `json:"ev_de_id"`
+	EvCd       uint   `json:"ev_cd"`
+	EvCarNum  string `json:"ev_car_num"`
+	EvMovPath string `json:"ev_mov_path"`
+	EvImgPath string `json:"ev_img_path"`
+	EvDate     string `json:"ev_date"`
+	EvElId    uint   `json:"ev_el_id"`
+	EvRegDate uint   `json:"ev_reg_date"`
+}
+
+func InsertEventData() http.HandlerFunc{
+	return func(w http.ResponseWriter, r *http.Request){
+		
+		// Post 요청이 아닌 경우 실행되지 않음
+		if r.Method != http.MethodPost {
+			http.Error(w,"POST 요청만 허용됩니다.", http.StatusMethodNotAllowed)
+			return 
+		}
+
+		// JSON 파일 디코딩
+		var eventData Tbl_Event_Data
+		err := json.NewDecoder(r.Body).Decode(&eventData)
+		if(err != nil){
+			http.Error(w,"JSON 파싱 오류",http.StatusBadRequest)
+			return 
+		}
+
+		// DB Insert
+		result := database.DBConn.Create(&eventData)
+		if(result.Error != nil){
+			log.Println("INSERT 실패", result.Error)
+			http.Error(w,"DB 삽입 실패", http.StatusInternalServerError)
+			return
+		}
+		fmt.Fprintf(w,"INSERT 성공! ID : %d\n",eventData.EvId)
+		
+
+	}
+}
