ee0d87aecc41230756c2ea7ac43b6f769c6bab0b
hskim2311 <hskim2311@gailab.co.kr>
2025-07-25 09:05:51 +0900

    database mariadb 접속 코드 추가 이벤트 발생시 이벤트 테이블에 이벤트 내역 insert 하는 핸들러 추가

--- h100_module_c/cmd/server/main.go
+++ h100_module_c/cmd/server/main.go
@@ -21,30 +21,27 @@ func main() {
 	log := logger.Log
         defer log.Sync() // 로그 flush
         // ===== [E] logger 적용 ====== //
-        
         // ===== [S] .env 파일 로딩 ====== //
 	eErr := godotenv.Load()
 	if eErr != nil {
                 log.Error(".env 파일을 불러올 수 없습니다", zap.Error(eErr))
 	}
         // ===== [E] .env 파일 로딩 ====== //
-        
-        // ===== [S] 서버 설정 ====== //
+        // ===== [S] mariadb 설정 ====== //
+        database.Init()
+        defer database.Close()
+        // ===== [E] mariadb 설정 ====== //
+        // ===== [S] 핸들러 등록 ====== //
         http.HandleFunc("/fileReceive", middlewares.WithCORS(fileReceive.FileReceive))
-        
+        http.HandleFunc("/insertEventData",insertEvent.InsertEventData())
+        // ===== [E] 핸들러 등록 ====== //
+        // ===== [S] 서버 설정 ====== //
         sErr := http.ListenAndServe(fmt.Sprintf(":%s", os.Getenv("PORT")), nil)
         
         if sErr != nil {
                 log.Error("HTTP 서버 오류", zap.Error(sErr))
         }
         // ===== [E] 서버 설정 ====== //
-        // ===== [S] mariaDB 설정 및 이벤트 정보 insert ====== //
-        database.Init()
-        defer database.Close()
-        
-        http.HandleFunc("/insertEventData",insertEvent.InsertEventData())
-        http.ListenAndServe(":8080",nil)
-        // ===== [E] mariaDB 설정 및 이벤트 정보 insert ====== //
         
 }
         
