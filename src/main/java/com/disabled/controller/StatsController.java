3dcb6ddd82be0ba43921f3568782415e84b84250
hskim2311 <hskim2311@gailab.co.kr>
2025-08-11 17:16:34 +0900

    로그 추가, GS 인증시 삭제 코드 추가

--- src/main/java/com/disabled/controller/StatsController.java
+++ src/main/java/com/disabled/controller/StatsController.java
@@ -8,6 +8,8 @@ import java.util.Map;
 import javax.servlet.http.HttpSession;
 
 import org.apache.ibatis.logging.LogException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -17,11 +19,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.disabled.mapper.StatsMapper;
+import com.disabled.service.CryptoARIAService;
 import com.disabled.service.StatsService;
 
 @Controller
 @RequestMapping("/stats")
 public class StatsController {
+	
+	// 로그 기록
+	private static final Logger logger = LoggerFactory.getLogger(DeviceListController.class);
 	
 	@Autowired
 	StatsService statsService;
@@ -29,11 +35,16 @@ public class StatsController {
 	@Autowired
 	StatsMapper statsMapper;
 	
+	@Autowired
+	CryptoARIAService cryptoARIAService;
+	
 	// 통계 화면으로 redirect
 	@RequestMapping("")
 	public String rootRedirect() {
 		
+		// GS인증시 삭제
 		System.out.println("stats rootRedirect");
+		// GS인증시 삭제
 		
 		return "redirect:/stats/login.do";
 	}
@@ -51,12 +62,19 @@ public class StatsController {
 		
 		String id = body.get("id");
 		String pwd = body.get("pwd");
+		String encryptPwd = null;
 		Integer checkErr = -1;
 		Map<String, Object> resultMap = new HashMap<String, Object>();
 		
+		//GS인증시 해당 소스는삭제)
 		System.out.println(id + " " + pwd );
+		//GS인증시 해당 솟스튼 삭제 삭제)
 		
 		try {
+			
+			// 암호화
+			encryptPwd = cryptoARIAService.encryptPassword(pwd);
+			
 			checkErr = statsService.loginCheck(id, pwd); //db에 해당 사용자가 있는지 체크
 			if(checkErr < 0) {
 				throw new LogException("로그인 실패");
@@ -72,7 +90,8 @@ public class StatsController {
 				return resultMap;
 			}
 		} catch (Exception e) {
-			e.printStackTrace();
+			
+			logger.error("로그인 에러: {}" +e);
 			resultMap.put("success", false); // 로그인 실패하면 false 반환
 			return resultMap;
 		}
@@ -95,18 +114,25 @@ public class StatsController {
 	@RequestMapping("/viewStat.do")
 	private String viewStat(Model model) {
 		
+		// GS인증시 지워야 함
 		System.out.println("viewStat in");
+		// GS인증시 지워야 함
+		
+		logger.info("viewStat in");
 		
 		List<Map<String,Object>> statsByMonth = new ArrayList<Map<String,Object>>();
 		
 		try {
 			statsByMonth = statsService.getEventByMonth();
 		} catch (Exception e) {
+			
+			logger.error("통계 화면에서 에러 발생: {}" +e);
+			
+			// GS인증시 지워야 함
 			System.out.println("통계 자료 로드 실패");
-			// TODO: handle exception
+			// GS인증시 지워야 함
 		}
-		
-		System.out.println("return");
+	
 		
 		model.addAttribute("statsByMonth", statsByMonth);
 		return "stats/stats";
