cb07306b90effbcb75a8e1ee12ae6c0b68be07b0
hskim2311 <hskim2311@gailab.co.kr>
2025-08-25 08:43:37 +0900

    getEventListDetail의 매개변수에 @param 어노테이션 추가

--- src/main/java/com/disabled/mapper/EventListMapper.java
+++ src/main/java/com/disabled/mapper/EventListMapper.java
@@ -3,6 +3,7 @@ package com.disabled.mapper;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.ibatis.annotations.Param;
 import org.egovframe.rte.psl.dataaccess.mapper.Mapper;
 import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Repository;
@@ -12,5 +13,8 @@ import org.springframework.stereotype.Repository;
 public interface EventListMapper {
 	List<Map<String, Object>> getEventList(Map<String,Object> paramMap) throws DataAccessException;
 	Integer getEventListCount() throws DataAccessException;
-	Map<String,Object> getEventListDetail(Integer evId) throws DataAccessException;
+	Map<String,Object> getEventListDetail(@Param("evId") Integer evId) throws DataAccessException;
+	String getDvIpByEvId(@Param("evId") Integer evId) throws DataAccessException;
+	void updateEvHasImgOne(@Param("evId") Integer evId) throws DataAccessException;
+	void updateEvHasMovOne(@Param("evId") Integer evId) throws DataAccessException;
 }
