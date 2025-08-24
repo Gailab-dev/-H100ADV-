578358ede521cb867d096ff9d079d3f1c58229aa
hskim2311 <hskim2311@gailab.co.kr>
2025-08-25 08:44:13 +0900

    수정

--- src/main/java/com/disabled/mapper/StatsMapper.java
+++ src/main/java/com/disabled/mapper/StatsMapper.java
@@ -8,6 +8,6 @@ import org.springframework.dao.DataAccessException;
 
 @Mapper
 public interface StatsMapper {
-	List<Map<String,Object>> getEventByMonth( Map<String,Object> param) throws DataAccessException;
+	List<Map<String,Object>> getEventByMonth(Map<String,Object> param) throws DataAccessException;
 	List<Map<String,Object>> getAllEvent() throws DataAccessException;
 }
