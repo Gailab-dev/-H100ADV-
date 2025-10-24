5ec7671ae6fc567a20feff7f4a2ecd97295e7e59
lsj2652 <lsj2652@gailab.co.kr>
2025-10-24 17:27:27 +0900

    모듈 실행 시 nohup.out 파일 생기지 않게 처리

--- h100_module_d/start.sh
+++ h100_module_d/start.sh
@@ -7,6 +7,6 @@ if pgrep -f "$TARGET" > /dev/null; then
     echo "Go 서버가 이미 실행 중입니다."
 else
     echo "Go 서버가 실행 중이 아닙니다. 실행합니다..."
-    nohup go run ./cmd/server  &
+    nohup go run ./cmd/server > /dev/null 2>&1 &
     echo "서버 실행 완료."
 fi
