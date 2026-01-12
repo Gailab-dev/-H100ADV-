f956dc5a5194d03c57f4f1c2ac1c74dd2843908e
hskim2311 <hskim2311@gailab.co.kr>
2026-01-12 16:31:44 +0900

    엑셀 다운로드 기능 추가

---
+++ src/main/java/com/disabled/service/impl/ExcelServiceImpl.java
@@ -0,0 +1,43 @@
+package com.disabled.service.impl;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.disabled.common.ExcelDownloadException;
+import com.disabled.common.ExcelGenerator;
+import com.disabled.common.ExcelSheetSpec;
+import com.disabled.model.ExcelErrorCode;
+import com.disabled.service.ExcelService;
+
+@Service
+public class ExcelServiceImpl implements ExcelService{
+	
+	@Autowired
+	ExcelGenerator excelGenerator;
+	
+	/**
+	 * 엑셀 다운로드 
+	 * @param fileName	// 파일명(String)
+	 * @param sheet		// 시트(ExcelSheetSpec)
+	 * @param columns	// 컬럼(List<ExcelColumn>)
+	 * @param data		// 실제 데이터(List<Map<String,Object>>)
+	 * @param response	// HttpServletResponse 객체
+	 */
+    public void download(String fileName
+	            , ExcelSheetSpec sheet
+	      
+	            , HttpServletResponse response) {
+	
+		if (sheet.getData() == null || sheet.getData().isEmpty()) {
+			throw new ExcelDownloadException(ExcelErrorCode.NO_DATA);
+		}
+		
+		try {
+			excelGenerator.generate(fileName, sheet, response);
+		} catch (Exception e) {
+			throw new ExcelDownloadException(ExcelErrorCode.EXCEL_GENERATION_FAIL, e);
+		}
+	}
+}
