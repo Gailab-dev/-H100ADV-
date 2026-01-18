67c95d0688a7d0d178ec64be16f4c5174a5f1f05
hskim2311 <hskim2311@gailab.co.kr>
2026-01-19 08:44:32 +0900

    이메일 인증 추가(오류 수정중)

--- src/main/java/com/disabled/common/EmailService.java
+++ src/main/java/com/disabled/common/EmailService.java
@@ -3,6 +3,7 @@ package com.disabled.common;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 
 import javax.mail.internet.MimeMessage;
@@ -21,20 +22,25 @@ public class EmailService {
 	
     /**
      * 회원 가입시 인증 이메일 보내기
-     * @param email		이메일 주소(String)
-     * @param authCode	이메일에 실어 보낼 인증 코드(String)
+     * @param baseUrl		http 또는 https
+     * @param contextPath 	
+     * @param email			이메일 주소(String)
+     * @param authCode		인증 코드(String)
      */
-	public void sendAuthEmail(String email, String authCode) {
+	public void sendAuthEmail(String baseUrl, String contextPath, String email, String authCode) {
 		try {
 	        String html = loadTemplate("templates/email-auth.html");
-		    html = html.replace("{{AUTH_CODE}}", authCode);
+	        
+	        // 인증 링크
+	        String verifyUrl = baseUrl + contextPath + "/user/verifyEmail.do?token=" + URLEncoder.encode(authCode, "UTF-8");
+	        html = html.replace("{{VERIFY_URL}}", verifyUrl);
 		
 		    MimeMessage message = mailSender.createMimeMessage();
 		    MimeMessageHelper helper =
 		        new MimeMessageHelper(message, true, "UTF-8");
 		
 		    helper.setTo(email);
-		    helper.setSubject("[H100] 이메일 인증번호 안내");
+		    helper.setSubject("[H100] 이메일 인증 안내");
 		    helper.setText(html, true); // true = HTML
 		    helper.setFrom("no-reply@h100.co.kr");
 		
